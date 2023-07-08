d21202ef4b5a98dcf7abf4156059243beb03c802
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-07-08 00:49:39 -0400

    BetterChat
    - New project upload

---
+++ BetterChat/src/main/java/com/rosed/betterchat/BetterChat.java
@@ -0,0 +1,13 @@
+package com.rosed.betterchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+}
