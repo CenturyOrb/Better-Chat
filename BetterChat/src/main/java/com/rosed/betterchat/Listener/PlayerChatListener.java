ccc84a18b59da5f8710627f7d225c4251d9bd48a
CenturyOrb <122176094+CenturyOrb@users.noreply.github.com>
2023-07-09 01:37:01 -0400

    Added Megaphone functionality
    - added megaphone logic to AsyncChatEvent

--- BetterChat/src/main/java/com/rosed/betterchat/Listener/PlayerChatListener.java
+++ BetterChat/src/main/java/com/rosed/betterchat/Listener/PlayerChatListener.java
@@ -8,6 +8,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,21 +32,37 @@ public class PlayerChatListener implements Listener {
     public void onPlayerChatEvent(AsyncChatEvent e)   {
 
         Player chatSender = e.getPlayer();
+        int radius = ConfigManager.getConfigRadius();
 
-        // cancel if sender is near no one
+        // check if the player is holding the megaphone
+        ItemMeta mainHandMeta = chatSender.getInventory().getItemInMainHand().getItemMeta();
+        ItemMeta offHandMeta = chatSender.getInventory().getItemInOffHand().getItemMeta();
+
+        // check mainhand for megaphone
+        if (mainHandMeta != null && mainHandMeta.getLocalizedName().equals("better-chat.megaphoneitem"))   { // checks if the item isn't the megaphone
+            radius = ConfigManager.getConfigMegaphoneRadius();
+        }
+
+        // check offhand for megaphone
+        if (offHandMeta != null && offHandMeta.getLocalizedName().equals("better-chat.megaphoneitem"))   { // checks if the item isn't the megaphone
+            radius = ConfigManager.getConfigMegaphoneRadius();
+        }
+
+        chatSender.sendMessage(ChatColor.GREEN + "Your chat radius is: " + radius);
+
+        // remove viewers if the viewer isn't close enough
         for (Player player : playerManager.getPlayers())   {
-            if (chatSender.getLocation().distance(player.getLocation()) > ConfigManager.getConfigRadius())   {
+            if (chatSender.getLocation().distance(player.getLocation()) > radius)   {
                 e.viewers().remove(player);
             }
         }
 
+        // send message if there are no viewers nearby
         if (0 >= e.viewers().size() - 2) {
             chatSender.sendMessage(ChatColor.RED + "No one heard your message");
             e.setCancelled(true);
         }
 
     }
-
-    public List<Player> getPlayers()   { return playerManager.getPlayers(); }
 
 }
